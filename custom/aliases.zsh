670bf06f86dbacd4614ca0c83496b7c8601e1d4c
Shouichi Kamiya <shouichi.kamiya@gmail.com>
2013-10-18 15:41:47 +0900

    alias gf = git fetch

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -13,12 +13,12 @@ alias su='su -'
 
 alias t='tree -ACFh'
 
-alias gs='git status'
-alias gg='git grep'
-
 alias tm='tmux -u'
 
 alias r=ruby
 alias p=python
 
+alias gs='git status'
+alias gg='git grep'
+alias gf='git fetch'
 alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
